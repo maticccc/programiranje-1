0e4aa3d5128ef07695818463e96468c3f2c5c4ab
maticccc <131895670+maticccc@users.noreply.github.com>
2024-12-09 16:31:32 +0000

    vaje 9/12/2024

--- 08-induktivni-tipi/vsota2.lean
+++ 08-induktivni-tipi/vsota2.lean
@@ -40,3 +40,48 @@ theorem vsoti_sta_enaki : vsota xs = vsota' xs :=
           _ = aux xs' x := by rw [aux_in_plus]
           _ = aux (x :: xs') 0 := by simp [aux]
           _ = vsota' (x :: xs') := by simp [vsota']
+
+def reverse {A : Type} : List A -> List A :=
+  fun xs =>
+    match xs with
+    | [] => []
+    | y :: ys => reverse (ys) ++ [y]
+
+def reverseAux {A : Type} : List A -> List A -> List A :=
+  fun xs => fun acc =>
+    match xs with
+    | [] => acc
+    | y :: ys => reverseAux ys (y :: acc)
+
+
+theorem pomozna {A : Type} : ∀ {lst : List A}, ∀ {acc : List A},
+reverseAux lst acc = (reverse lst) ++ acc :=
+  by
+    -- nujen korak pred indukcijo
+    -- ključno je, da delamo indukcijo na lst
+    intro lst
+    induction lst with
+    -- lažji del
+    | nil =>
+      intro acc
+      simp[reverseAux, reverse]
+    -- težji del
+    | cons x xs ih =>
+      intro acc
+      simp [reverseAux]
+      rw [ih]
+      simp [reverse]
+
+
+def reverse' {A : Type} : List A -> List A :=
+  fun xs =>
+    reverseAux xs []
+
+-- zdaj rabimo dokazati samo še eno stvar
+
+theorem reverse_eq_reverse' {A : Type} {xs : List A} : reverse xs = reverse' xs :=
+  by
+    simp [reverse']
+    rw [pomozna]
+    -- zadnji korak je še, da pokličeš trditev ki pravi, da je: "xs = xs @ []"
+    sorry
